3cdbc510263dfd21a7ace353d698cbdb6af68dce
Yuliya Abramovitch <yuabramovitch@gmail.com>
2024-03-25 21:22:00 +0300

    Add methods in ProductSearchPage, BasketPage Classes

--- src/main/java/by/yvesrocher/ui/BasketPage.java
+++ src/main/java/by/yvesrocher/ui/BasketPage.java
@@ -1,8 +1,32 @@
 package by.yvesrocher.ui;
 
+import by.yvesrocher.driver.Driver;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BasketPage {
+    WebDriver driver;
+
+    public BasketPage() {
+        this.driver = Driver.getDriver();
+    }
+
     private By chosenProduct = By.cssSelector(".chosen-products .chosen-product");
     private By getChosenProductName = By.cssSelector(".product-info .name");
+
+    public List<WebElement> getChosenItems() {
+        return driver.findElements(chosenProduct);
+    }
+
+    private List<String> getItemNames(List<WebElement> chosenItems) {
+        List<String> itemNames = new ArrayList<>();
+        for (WebElement chosenItem : chosenItems) {
+            itemNames.add(chosenItem.findElement(getChosenProductName).getText());
+        }
+        return itemNames;
+    }
 }
--- src/main/java/by/yvesrocher/ui/ProductSearchPage.java
+++ src/main/java/by/yvesrocher/ui/ProductSearchPage.java
@@ -1,17 +1,39 @@
 package by.yvesrocher.ui;
 
+import by.yvesrocher.driver.Driver;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
 public class ProductSearchPage {
+    WebDriver driver;
+
+    public ProductSearchPage() {
+        this.driver = Driver.getDriver();
+    }
+
     private By searchField = By.cssSelector(".search .eye-placeholder");
     private By searchMagnifier = By.cssSelector("div[aria-label='search-btn']");
     private By searchResultsCounter = By.cssSelector(".heading-wrapper .number-of-products");
     private By item = By.cssSelector(".products .product-item");
     private By itemName = By.cssSelector(".product-item .card-name");
     private By priceScrollBar = By.cssSelector(".slider .price-scrollbar");
+    private By minPrice = By.cssSelector(".prices-range .min");
+    private By maxPrice = By.cssSelector(".prices-range .max");
     private By lowerRate = By.cssSelector(".noUi-handle-lower");
     private By upperRate = By.cssSelector(".noUi-handle-upper");
     private By basketBtn = By.cssSelector(".basket-btn");
     private By redirectToBasketBtn = By.cssSelector(".modal-basket .basket-btn");
 
+    public void searchInCatalog(String searchWord) {
+        driver.findElement(searchField).sendKeys(searchWord);
+        driver.findElement(searchMagnifier).click();
+    }
+
+    public String getItemName() {
+        return driver.findElement(itemName).getText();
+    }
+
+    public void goToBasket() {
+        driver.findElement(redirectToBasketBtn).click();
+    }
 }
